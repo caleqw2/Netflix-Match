bbcad19297dba42276195ddda8c6df1e15d58ef2
caleqw2 <caleqw2@illinois.edu>
2021-04-25 17:44:07 -0500

    made a buncha changes

--- app.py
+++ app.py
@@ -7,198 +7,263 @@ app = Flask(__name__)
 CORS(app)
 
 # /create_user_watch_list_entry?user_id=0&media_id=0&watched=true
+
+
 @app.route('/create_user_watch_list_entry')
 def create_user_watch_list_entry():
-  user_id = get_user_id(request.args.get('user_name'))
-  media_id = get_media_id(request.args.get('media_name'))
-  watched = request.args.get('watched')
+    user_id = get_user_id(request.args.get('user_name'))
+    media_id = get_media_id(request.args.get('media_name'))
+    watched = request.args.get('watched')
 
-  if user_id == "Does not exist":
-    return {'result': "User doesn't exist"}
+    if user_id == "Does not exist":
+        return {'result': "User doesn't exist"}
 
-  if media_id == "Does not exist":
-    return {'result': "Media doesn't exist"}
+    if media_id == "Does not exist":
+        return {'result': "Media doesn't exist"}
 
-  mydb = msc.connect(
-    host="104.197.38.184", # This is the IP of the GCP instance
-    user="root",
-    password="12345",
-    database="netflix_match"
-  )
+    mydb = msc.connect(
+        host="104.197.38.184",  # This is the IP of the GCP instance
+        user="root",
+        password="12345",
+        database="netflix_match"
+    )
 
-  mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-  mycursor.execute("INSERT INTO UserWatchlistEntry (user_id, media_id, watched) VALUES ({}, {}, {})".format(user_id, media_id, watched))
+    mycursor.execute("INSERT INTO UserWatchlistEntry (user_id, media_id, watched) VALUES ({}, {}, {})".format(
+        user_id, media_id, watched))
 
-  mycursor.execute("SELECT username, media_title, watched FROM UserWatchlistEntry NATURAL JOIN User Natural JOIN Media WHERE user_id = '{}' AND media_id = '{}'".format(user_id, media_id))
-  myresult = mycursor.fetchall()
+    mycursor.execute("SELECT username, media_title, watched FROM UserWatchlistEntry NATURAL JOIN User Natural JOIN Media WHERE user_id = '{}' AND media_id = '{}'".format(user_id, media_id))
+    myresult = mycursor.fetchall()
 
-  mydb.commit()
-  mydb.close()
+    mydb.commit()
+    mydb.close()
 
-  watched_str = "false"
-  if (myresult[0][2]):
-    watched_str = "true"
+    watched_str = "false"
+    if (myresult[0][2]):
+        watched_str = "true"
 
-  result_str = "Username=" + myresult[0][0] + " Media Title=" + myresult[0][1] + " Watched=" + watched_str
-  return {'result': result_str}
+    result_str = "Username=" + \
+        myresult[0][0] + " Media Title=" + \
+        myresult[0][1] + " Watched=" + watched_str
+    return {'result': result_str}
 
 # /look_up_show_name?keywork=abcd
+
+
 @app.route('/look_up_show_name')
 def look_up_show_name():
-  keyword = request.args.get('keyword')
+    keyword = request.args.get('keyword')
 
-  mydb = msc.connect(
-    host="104.197.38.184", # This is the IP of the GCP instance
-    user="root",
-    password="12345",
-    database="netflix_match"
-  )
+    mydb = msc.connect(
+        host="104.197.38.184",  # This is the IP of the GCP instance
+        user="root",
+        password="12345",
+        database="netflix_match"
+    )
 
-  mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-  mycursor.execute("SELECT media_title FROM Media WHERE media_title LIKE '%{}%'".format(keyword))
+    mycursor.execute(
+        "SELECT media_title FROM Media WHERE media_title LIKE '%{}%'".format(keyword))
 
-  myresult = mycursor.fetchall()
+    myresult = mycursor.fetchall()
 
-  mydb.commit()
-  mydb.close()
+    mydb.commit()
+    mydb.close()
 
-  result_str = ""
-  for row in myresult:
-    result_str += row[0] + "; "
-  
-  return {'result': result_str}
+    result_str = ""
+    for row in myresult:
+        result_str += row[0] + "; "
+
+    return {'result': result_str}
 
 # /update_user_watch_list_entry?user_name=0&media_name=0&watched=true
+
+
 @app.route('/update_user_watch_list_entry')
 def update_user_watch_list_entry():
-  user_id = get_user_id(request.args.get('user_name'))
-  media_id = get_media_id(request.args.get('media_name'))
-  watched = request.args.get('watched')
+    user_id = get_user_id(request.args.get('user_name'))
+    media_id = get_media_id(request.args.get('media_name'))
+    watched = request.args.get('watched')
 
-  if user_id == "Does not exist":
-    return {'result': "User doesn't exist"}
+    if user_id == "Does not exist":
+        return {'result': "User doesn't exist"}
 
-  if media_id == "Does not exist":
-    return {'result': "Media doesn't exist"}
+    if media_id == "Does not exist":
+        return {'result': "Media doesn't exist"}
 
-  mydb = msc.connect(
-    host="104.197.38.184", # This is the IP of the GCP instance
-    user="root",
-    password="12345",
-    database="netflix_match"
-  )
+    mydb = msc.connect(
+        host="104.197.38.184",  # This is the IP of the GCP instance
+        user="root",
+        password="12345",
+        database="netflix_match"
+    )
 
-  mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-  mycursor.execute("UPDATE UserWatchlistEntry SET watched = {} WHERE user_id = {} AND media_id = {}".format(watched, user_id, media_id))
+    mycursor.execute("UPDATE UserWatchlistEntry SET watched = {} WHERE user_id = {} AND media_id = {}".format(
+        watched, user_id, media_id))
 
-  mycursor.execute("SELECT username, media_title, watched FROM UserWatchlistEntry NATURAL JOIN User Natural JOIN Media WHERE user_id = '{}' AND media_id = '{}'".format(user_id, media_id))
-  myresult = mycursor.fetchall()
+    mycursor.execute("SELECT username, media_title, watched FROM UserWatchlistEntry NATURAL JOIN User Natural JOIN Media WHERE user_id = '{}' AND media_id = '{}'".format(user_id, media_id))
+    myresult = mycursor.fetchall()
 
-  mydb.commit()
-  mydb.close()
+    mydb.commit()
+    mydb.close()
 
-  watched_str = "false"
-  if (myresult[0][2]):
-    watched_str = "true"
+    watched_str = "false"
+    if (myresult[0][2]):
+        watched_str = "true"
 
-  result_str = "Username=" + myresult[0][0] + " Media Title=" + myresult[0][1] + " Watched=" + watched_str
-  return {'result': result_str}
+    result_str = "Username=" + \
+        myresult[0][0] + " Media Title=" + \
+        myresult[0][1] + " Watched=" + watched_str
+    return {'result': result_str}
 
 # /delete_user_watch_list_entry?user_id=0&media_id=0
+
+
 @app.route('/delete_user_watch_list_entry')
 def delete_user_watch_list_entry():
-  user_id = get_user_id(request.args.get('user_name'))
-  media_id = get_media_id(request.args.get('media_name'))
+    user_id = get_user_id(request.args.get('user_name'))
+    media_id = get_media_id(request.args.get('media_name'))
 
-  if user_id == "Does not exist":
-    return {'result': "User doesn't exist"}
+    if user_id == "Does not exist":
+        return {'result': "User doesn't exist"}
 
-  if media_id == "Does not exist":
-    return {'result': "Media doesn't exist"}
+    if media_id == "Does not exist":
+        return {'result': "Media doesn't exist"}
 
-  mydb = msc.connect(
-    host="104.197.38.184", # This is the IP of the GCP instance
-    user="root",
-    password="12345",
-    database="netflix_match"
-  )
+    mydb = msc.connect(
+        host="104.197.38.184",  # This is the IP of the GCP instance
+        user="root",
+        password="12345",
+        database="netflix_match"
+    )
 
-  mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-  mycursor.execute("DELETE FROM UserWatchlistEntry WHERE user_id = {} AND media_id = {}".format(user_id, media_id))
+    mycursor.execute(
+        "DELETE FROM UserWatchlistEntry WHERE user_id = {} AND media_id = {}".format(user_id, media_id))
 
-  mydb.commit()
-  mydb.close()
+    mydb.commit()
+    mydb.close()
 
-  user_name = request.args.get('user_name')
-  media_name = request.args.get('media_name').replace("%20", " ")
-  return {'result': "Deleted {} from {}'s watchlist".format(media_name, user_name)}
+    user_name = request.args.get('user_name')
+    media_name = request.args.get('media_name').replace("%20", " ")
+    return {'result': "Deleted {} from {}'s watchlist".format(media_name, user_name)}
+
 
 def get_media_id(media_name):
-  mydb = msc.connect(
-    host="104.197.38.184", # This is the IP of the GCP instance
-    user="root",
-    password="12345",
-    database="netflix_match"
-  )
+    mydb = msc.connect(
+        host="104.197.38.184",  # This is the IP of the GCP instance
+        user="root",
+        password="12345",
+        database="netflix_match"
+    )
 
-  media_name.replace("%20", " ")
+    media_name.replace("%20", " ")
 
-  mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-  mycursor.execute("SELECT media_id FROM Media WHERE media_title = '{}'".format(media_name))
+    mycursor.execute(
+        "SELECT media_id FROM Media WHERE media_title = '{}'".format(media_name))
 
-  myresult = mycursor.fetchall()
-  if not myresult:
-    return "Does not exist"
+    myresult = mycursor.fetchall()
+    if not myresult:
+        return "Does not exist"
 
-  mydb.commit()
-  mydb.close()
+    mydb.commit()
+    mydb.close()
 
-  return myresult[0][0]
+    return myresult[0][0]
+
 
 def get_user_id(user_name):
-  mydb = msc.connect(
-    host="104.197.38.184", # This is the IP of the GCP instance
-    user="root",
-    password="12345",
-    database="netflix_match"
-  )
+    mydb = msc.connect(
+        host="104.197.38.184",  # This is the IP of the GCP instance
+        user="root",
+        password="12345",
+        database="netflix_match"
+    )
 
-  mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-  mycursor.execute("SELECT user_id FROM User WHERE username = '{}'".format(user_name))
+    mycursor.execute(
+        "SELECT user_id FROM User WHERE username = '{}'".format(user_name))
 
-  myresult = mycursor.fetchall()
-  if not myresult:
-    return "Does not exist"
+    myresult = mycursor.fetchall()
+    if not myresult:
+        return "Does not exist"
 
-  mydb.commit()
-  mydb.close()
+    mydb.commit()
+    mydb.close()
 
-  return myresult[0][0]
+    return myresult[0][0]
+
 
 @app.route('/display_watchlist')
-  def display_watchlist():
-  mydb = msc.connect(
-    host="104.197.38.184", # This is the IP of the GCP instance
-    user="root",
-    password="12345",
-    database="netflix_match"
-  )
+def display_watchlist():
+    mydb = msc.connect(
+        host="104.197.38.184",  # This is the IP of the GCP instance
+        user="root",
+        password="12345",
+        database="netflix_match"
+    )
 
-  mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-  mycursor.execute("SELECT * FROM UserWatchlistEntry")
+    mycursor.execute("SELECT * FROM UserWatchlistEntry")
 
-  myresult = mycursor.fetchall()
-  if not myresult:
-    return "Does not exist"
+    myresult = mycursor.fetchall()
+    if not myresult:
+        return "Does not exist"
 
-  mydb.commit()
-  mydb.close()
+    mydb.commit()
+    mydb.close()
 
-  return myresult[0][0]
+    return myresult[0][0]
+
+
+@app.route('/get_question_info/<questionNumber>')
+def get_question_info(questionNumber):
+    mydb = msc.connect(
+        host="104.197.38.184",  # This is the IP of the GCP instance
+        user="root",
+        password="12345",
+        database="netflix_match"
+    )
+    mycursor = mydb.cursor()
+
+    results = {}
+
+
+    mycursor.execute(f"SELECT description FROM QuizQuestion WHERE question_id = {questionNumber}")
+    question_text = mycursor.fetchall()
+    print(question_text)
+
+    mycursor.execute(f"SELECT description, answer_choice_id FROM QuizAnswerChoice WHERE question_id = {questionNumber}")
+    answer_choices = mycursor.fetchall()
+    mydb.commit()
+    mydb.close()
+
+    results = {
+      'questionText': question_text[0],
+      'choiceOneText': answer_choices[0][0],
+      'choiceTwoText': answer_choices[1][0],
+      'choiceThreeText': answer_choices[2][0],
+      'choiceOneID': answer_choices[0][1],
+      'choiceTwoID': answer_choices[1][1],
+      'choiceThreeID': answer_choices[2][1],
+    }
+
+    print("running query")
+    return results
+
+    # return {
+    #   'questionText': 'Favorite Meal!',
+    #   'choiceOneText': 'Breakfast',
+    #   'choiceTwoText': 'Lunch',
+    #   'choiceThreeText': 'Dinner'
+    # }
+
--- netflix-match/src/components/Quiz.js
+++ netflix-match/src/components/Quiz.js
@@ -1,41 +1,75 @@
-import React,{useState, useEffect,useContext, Component} from 'react';
+import React, { useState, useEffect, useContext, Component } from 'react';
 import { Container, Row, Col } from 'react-grid';
 import '../App.css';
-import NavigationBar from './components/NavigationBar.js';
+import NavigationBar from './NavigationBar.js';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 class QuizCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+    
     render() {
-        const TITLE = 'My Page Title';
-    
         return (
-      
-<div className='info-block'> 
-    <div className='info-header'>
-        
-    </div>
-    
-    <div className='post1text'>
-      <Link to="/Home">
-        <strong>Mike Jones:</strong>
-        Just received my SAP iXp swag! Can't wait to wear this 
-        when I go back to school. Thank you SAP!!
-      </Link>
-    </div>
-</div>
-    );
-}} 
+            <div className='info-block'>
+                <h2>{this.props.optionText}</h2>
+            </div>
+        );
+    }
+}
 
 class Quiz extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentQuestion: 1,
+            questionText: 'Placeholder',
+            choiceOneText: '',
+            choiceTwoText: '',
+            choiceThreeText: '',
+            choiceOneID: -1,
+            choiceTwoID: -1,
+            choiceThreeID: -1
+        }
+
+        this.handleFetchQuestionChoiceInfo = this.handleFetchQuestionChoiceInfo.bind(this);
+    }
+
+    // Grab the question title and text from three answer choices
+    async handleFetchQuestionChoiceInfo(event) {
+        const url = `/get_question_info/${this.state.currentQuestion}`
+        fetch(url).then(res => res.json()).then(data => {
+            this.setState({
+                questionText: data.questionText,
+                choiceOneText: data.choiceOneText,
+                choiceTwoText: data.choiceTwoText,
+                choiceThreeText: data.choiceThreeText,
+                choiceOneID: data.choiceOneID,
+                choiceTwoID: data.choiceTwoID,
+                choiceThreeID: data.choiceThreeID
+            });
+        });
+    }
+
+    async componentDidMount() {
+        // Call some function to populate currentQuestion
+        this.handleFetchQuestionChoiceInfo(this.state.currentQuestion);
+    }
+
     render() {
-        <div>
-            <h1>Question 1</h1>
-            <h2>What is your favorite genre?</h2>
-            <NavigationBar />
-            <QuizCard />
-            <QuizCard />
-        </div>
+        console.log(this.state.choiceOneText);
+        return (
+            <div>
+                <h1>Question {this.state.currentQuestion}</h1>
+                <h2>{this.state.questionText}</h2>
+                <QuizCard optionText={this.state.choiceOneText}/>
+                <QuizCard optionText={this.state.choiceTwoText}/>
+                <QuizCard optionText={this.state.choiceThreeText}/>
+            </div>
+        );
     };
 }
 
-export default {QuizCard, Quiz};
+export default Quiz;
